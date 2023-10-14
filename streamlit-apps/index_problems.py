3d8f1d45e0f32d72cc29b1c0d665fe292599b803
iuruoy-shao <youruishao115022@gmail.com>
2023-10-13 21:52:51 -0700

    update model

--- streamlit-apps/index_problems.py
+++ streamlit-apps/index_problems.py
@@ -11,10 +11,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from gather_problems import base_url
 from transformers import DistilBertTokenizer, DistilBertModel
 from huggingface_hub import hf_hub_download
+import nltk
+from nltk.corpus import stopwords
 
 from io import StringIO
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
+
+nltk.download('stopwords')
+nltk_stopwords = set(stopwords.words('english'))
 
 REPO_ID = 'iuruoy-shao/top-level-with-solutions-distilbert-amc10-2019-2022'
 FILENAME = 'top-level-with-solutions-distilbert-amc10-2019-2022.pt'
@@ -40,7 +45,7 @@ st.set_page_config(page_title="Categorizing AMC Problems",
                    menu_items={"About":"""This is a demonstration of our top-level categorization model for competition math problems at the highschool level.
                                The model can be found at https://huggingface.co/iuruoy-shao/top-level-with-solutions-distilbert-amc10-2019-2022."""})
 
-problems_data = json.load(open('../amc_10_problems.json'))
+problems_data = json.load(open('../amc_10_problems_with_sol.json'))
 categories = ['Miscellaneous', 'Algebra', 'Geometry', 'Number Theory', 'Counting and Probability']
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -67,25 +72,6 @@ def make_prediction(sequence):
     return [[1 if value else 0 for value in output] for output in outputs]
 
 problem, results = st.columns(2,gap='large')
-
-with problem:
-    st.title("Categorize AMC Problems")
-
-    tests = [[year,10,instance] for year in [2015, 2016, 2017, 2018, 2019, 2020, 2021, "2021_Fall", 2022] for instance in ["A","B"]][::-1]
-    tests_display = [f"{test[0]} AMC 10{test[2]}".replace("_"," ") for test in tests]
-
-    test_index = tests_display.index(st.selectbox(label="Test", options=tests_display, index=17))
-    test = tests[test_index]
-    problem_number = st.selectbox(label="Problem Number", 
-                                  index=15,
-                                  options=[problems_data[problem]['number'] for problem in problems_data 
-                                           if problems_data[problem]['test'] == test])
-
-    problem = f'{tests_display[test_index]} #{problem_number}'.replace(" Fall","_Fall")
-    problem_content = problems_data[problem]['problem']
-    st.write(f"[View problem on AoPS]({base_url}{tests_display[test_index].replace(' ','_')}_Problems/Problem_{problem_number})")
-    st.components.v1.html(problem_content,height=250,scrolling=True)
-    categorize = st.button(label="Categorize")
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -114,7 +100,33 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+def remove_stopwords(text):
+    filtered_text = [w for w in text.split() if w.lower() not in nltk_stopwords]
+    return " ".join(filtered_text)
+
 strip_problem_html = lambda x: strip_tags(get_latex_from_alt(x))
+
+with problem:
+    st.title("Categorize AMC Problems")
+
+    tests = [[year,10,instance] for year in [2015, 2016, 2017, 2018, 2019, 2020, 2021, "2021_Fall", 2022] for instance in ["A","B"]][::-1]
+    tests_display = [f"{test[0]} AMC 10{test[2]}".replace("_"," ") for test in tests]
+
+    test_index = tests_display.index(st.selectbox(label="Test", options=tests_display, index=17))
+    test = tests[test_index]
+    problem_number = st.selectbox(label="Problem Number", 
+                                  index=15,
+                                  options=[problems_data[problem]['number'] for problem in problems_data 
+                                           if problems_data[problem]['test'] == test])
+
+    problem = f'{tests_display[test_index]} #{problem_number}'.replace(" Fall","_Fall")
+    problem_content = problems_data[problem]['problem']
+    solutions_text = strip_tags(get_latex_from_alt(" ".join([solution for solution in problems_data[problem]["solutions"] if 'http' not in solution])))
+    choices_text = " ".join(problems_data[problem]["choices"])
+    combined_text = remove_stopwords(" ".join([problem_content, solutions_text, choices_text]))
+    st.write(f"[View problem on AoPS]({base_url}{tests_display[test_index].replace(' ','_')}_Problems/Problem_{problem_number})")
+    st.components.v1.html(problem_content,height=250,scrolling=True)
+    categorize = st.button(label="Categorize")
 
 with results:
     if categorize:
