7bbf187d37c53ac6a16e6a89437d8ee6da488b4d
iuruoy-shao <youruishao115022@gmail.com>
2023-12-03 23:49:48 -0800

    added profiles to database + pages

--- site/app.py
+++ site/app.py
@@ -1,7 +1,9 @@
 import contextlib
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from flask_migrate import Migrate
 from datetime import datetime
 from operator import attrgetter
 import validate_email
@@ -13,7 +15,17 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'amc10_problems.db')
 app.config['SECRET_KEY'] = "test"
-db = SQLAlchemy(app)
+convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
@@ -76,7 +88,7 @@ class ProblemHistory(db.Model):
     completion = db.Column(db.Integer, nullable=False, default=0)
     last_attempted = db.Column(db.String)
     previous_answers = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
 
     def __repr__(self):
         return f'id: {self.problem_id} \nattempts: {self.attempts}'
@@ -93,16 +105,37 @@ class ProblemHistory(db.Model):
             self.last_attempted = datetime.now()
             self.attempts += 1
         self.previous_answers = json.dumps(answers)
+        current_user._current_profile().last_active = datetime.now()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True, nullable=False)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-    problems_history = db.relationship('ProblemHistory', backref='user', lazy='dynamic')
+    profiles = db.relationship('Profile', backref='user', lazy='dynamic')
+    current_profile = db.Column(db.Integer, nullable=False, default=1)
+    
+    def _current_profile(self):
+        '''
+        Returns the Profile object with id corresponding to the value of current_profile
+        '''
+        return Profile.query.get_or_404(self.current_profile)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    problems_history = db.relationship('ProblemHistory', backref='profile', lazy='dynamic')
     performance = db.Column(db.String(2000), nullable=False)
     preferred_categories = db.Column(db.String(2000), nullable=False, default='[]') # whitelisted categories (list)
-    
+    date_created = db.Column(db.String)
+    last_active = db.Column(db.String)
+
+    def __repr__(self):
+        return f"""{self.name}: {self.total_completed()} problems completed, 
+        created on {datetime.strptime(self.date_created, "%Y-%m-%d %H:%M:%S.%f").date()}, 
+        last active on {datetime.strptime(self.last_active, "%Y-%m-%d %H:%M:%S.%f").date()}"""
+
     def _performance(self,category=None):
         parsed_performance = json.loads(self.performance.replace("\'", "\""))
         return parsed_performance[category] if category else parsed_performance
@@ -154,6 +187,8 @@ class User(UserMixin, db.Model):
         # return [in_category.order_by(ProblemHistory.last_attempted.desc()).limit(n)]
         # return max(in_category,key=attrgetter('last_attempted'))
         return sorted(in_category, key=attrgetter('last_attempted'))[:n]
+    def total_completed(self):
+        return self.problems_history.filter_by(completion=1).count()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -183,12 +218,15 @@ def register():
         if not errors:
             user = User(email=request.form.get('email'),
                         username=request.form.get('username'),
-                        password=request.form.get('password'),
-                        performance = str({category : {"score":50.0,"status":0,"completed":0} 
-                                           for category in AllStatistics.query.first().category_names()}),
-                        preferred_categories = json.dumps(AllStatistics.query.first().category_names())
-                        )
+                        password=request.form.get('password'))
+            default_profile = Profile(name = f"{request.form.get('username')} default", 
+                                      user_id = current_user.id,
+                                      performance = str({category : {"score":50.0,"status":0,"completed":0} for category in AllStatistics.query.first().category_names()}),
+                                      preferred_categories = json.dumps(AllStatistics.query.first().category_names()),
+                                      date_created = datetime.now(),
+                                      last_active = datetime.now())
             db.session.add(user)
+            db.session.add(default_profile)
             db.session.commit()
             return redirect(url_for('login'))
         else:
@@ -211,11 +249,31 @@ def login():
             return render_template('login.html',user_not_found=True)
     return render_template('login.html',user_not_found=False)
 
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    if request.method == 'POST':
+        if 'change_profile' in request.form:
+            new_profile = request.form['profile_choices']
+            current_user.current_profile = new_profile
+            db.session.commit()
+        else:
+            profile_name = request.form.get('profile_name')
+            new_profile = Profile(name = profile_name, 
+                                  user_id = current_user.id,
+                                  performance = str({category : {"score":50.0,"status":0,"completed":0} for category in AllStatistics.query.first().category_names()}),
+                                  preferred_categories = json.dumps(AllStatistics.query.first().category_names()),
+                                  date_created = datetime.now(),
+                                  last_active = datetime.now())
+            db.session.add(new_profile)
+            db.session.commit()
+    return render_template('profile.html', profiles=current_user.profiles, current_profile=current_user.current_profile)
+
 @app.route('/')
 @login_required
 def index():
     try:
-        problem_id = current_user.problems_history.order_by(ProblemHistory.id.desc()).limit(1)[0].problem_id
+        problem_id = current_user._current_profile().problems_history.order_by(ProblemHistory.id.desc()).limit(1)[0].problem_id
     except Exception:
         problem_id = 1
     return redirect(f'/problems/{problem_id}')
@@ -224,14 +282,14 @@ def index():
 @login_required
 def render(problem_id):
     problem = Problem.query.get_or_404(problem_id)
-    if not current_user.problems_history.filter_by(problem_id=problem_id).first():
-        db.session.add(ProblemHistory(problem_id=problem_id, user_id=current_user.id))
+    if not current_user._current_profile().problems_history.filter_by(problem_id=problem_id).first():
+        db.session.add(ProblemHistory(problem_id=problem_id, profile_id=current_user.current_profile))
         db.session.commit()
-    problem_history = current_user.problems_history.filter_by(problem_id=problem_id).first()
+    problem_history = current_user._current_profile().problems_history.filter_by(problem_id=problem_id).first()
 
     # Updating preferred categories
     if request.method == 'POST' and 'categories' in request.form:
-        current_user.preferred_categories = json.dumps(request.form.getlist('categories'))
+        current_user._current_profile().preferred_categories = json.dumps(request.form.getlist('categories'))
         db.session.commit()
     else:
         if request.method == 'POST' and problem_history.completion == 0:
@@ -249,19 +307,19 @@ def render(problem_id):
                     problem=problem,
                     problem_history=problem_history,
                     allstatistics=AllStatistics.query.first(),
-                    user=current_user)
+                    profile=current_user._current_profile())
 
 @app.route('/next_problem/<int:ph_id>')
 @login_required
 def next_problem(ph_id):
-    problem_history = current_user.problems_history.filter_by(id=ph_id).first()
+    problem_history = current_user._current_profile().problems_history.filter_by(id=ph_id).first()
     problem = Problem.query.get_or_404(problem_history.problem_id)
     labels = problem._labels()
     problem_score = (5-problem_history.attempts)*25 * (1 + (problem.difficulty - 1)*.25)
     
     for label in label_to_categories(labels):
-        current_user.update_cat_score(label,problem_score)
-        current_user.update_cat_completion(label)
+        current_user._current_profile().update_cat_score(label,problem_score)
+        current_user._current_profile().update_cat_completion(label)
         db.session.commit()
     
     return redirect(url_for('recommend_problem'))
@@ -272,10 +330,10 @@ def recommend_problem():
     # picks category based on weighted probability and non-mastery
     categories = []
     scores = []
-    for category in current_user._performance():
-        if current_user._performance()[category]['status'] < 3 and category in current_user._preferred_categories():
+    for category in current_user._current_profile()._performance():
+        if current_user._current_profile()._performance()[category]['status'] < 3 and category in current_user._current_profile()._preferred_categories():
             categories.append(category)
-            scores.append(current_user._performance()[category]['score'])
+            scores.append(current_user._current_profile()._performance()[category]['score'])
     if not max(scores):
         weights = [1 for _ in scores]
     else:
@@ -284,11 +342,11 @@ def recommend_problem():
     category = random.choices(categories,weights)[0]
 
     # Picks a problem with difficulty matching the status
-    last_attempted = current_user.get_last_attempted(1,category)
+    last_attempted = current_user._current_profile().get_last_attempted(1,category)
 
     if last_attempted:
         last_completed_difficulty = Problem.query.get_or_404(last_attempted[0].problem_id).difficulty
-        match current_user._performance()[category]['status']:
+        match current_user._current_profile()._performance()[category]['status']:
             case 1:
                 difficulty = last_completed_difficulty - 1 if last_completed_difficulty > 1 else 1
             case 2:
@@ -299,7 +357,6 @@ def recommend_problem():
 
     return redirect(url_for('render', problem_id = next_problem_id))
 
-
 def query_problems(category, difficulty=3, approx_difficulty=True, allow_completed=True):
     """
     Returns the id of a problem with the given category
@@ -308,12 +365,12 @@ def query_problems(category, difficulty=3, approx_difficulty=True, allow_complet
     """
     # filter_difficulties = lambda x : Problem.query.filter(Problem.in_category(category),
     #                                                       Problem.difficulty in x,
-    #                                                       current_user.problems_history.filter_by(problem_id=Problem.id).first().attempts > 0).all()
+    #                                                       current_user.current_profile.problems_history.filter_by(problem_id=Problem.id).first().attempts > 0).all()
     filter_difficulties = lambda x : [problem for problem in Problem.query.all() 
                                       if problem.in_category(category) 
                                       and problem.difficulty in x 
-                                      and (not current_user.problems_history.filter_by(problem_id=problem.id).first() 
-                                           or not current_user.problems_history.filter_by(problem_id=problem.id).first().completion)]
+                                      and (not current_user._current_profile().problems_history.filter_by(problem_id=problem.id).first() 
+                                           or not current_user._current_profile().problems_history.filter_by(problem_id=problem.id).first().completion)]
     difficulties = [difficulty]
     filtered = filter_difficulties(difficulties)
 
